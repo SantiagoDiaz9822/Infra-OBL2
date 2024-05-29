386f3165b89c69545fb2c1d1bd9bdfec40f5e47d
Santiago Diaz <santiagodiaz9822@gmail.com>
2024-05-29 18:55:31 -0300

    lambda web-site funcionando

--- Terraform/lambda_function/lambda_function.py
+++ Terraform/lambda_function/lambda_function.py
@@ -1,5 +1,25 @@
+import boto3
+import os
+
+s3 = boto3.client('s3')
+
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda!'
-    }
+    bucket_name = os.environ['BUCKET_NAME']
+    key = 'index.html'
+    
+    try:
+        response = s3.get_object(Bucket=bucket_name, Key=key)
+        content = response['Body'].read().decode('utf-8')
+        
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'text/html'
+            },
+            'body': content
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -105,14 +105,25 @@ resource "aws_lambda_function" "static_site_lambda" {
   filename      = "./lambda_function.zip"
   function_name = "static-site-lambda"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
 }
 
 # Crear el rol IAM para la ejecución de la función Lambda
 resource "aws_iam_role" "lambda_role" {
   name               = "lambda_role"
-  assume_role_policy = data.aws_iam_policy_document.lambda_role_assume.json
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 # Documento de política para permitir a Lambda asumir el rol
@@ -127,77 +138,50 @@ data "aws_iam_policy_document" "lambda_role_assume" {
 }
 
 # Configurar la Política de Bucket S3
-data "aws_caller_identity" "current" {}
-
-data "aws_iam_policy_document" "s3_lambda_policy" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["arn:aws:s3:::${aws_s3_bucket.static_website.id}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [data.aws_caller_identity.current.account_id]
-    }
-  }
-}
-
 resource "aws_s3_bucket_policy" "lambda_access_policy" {
   bucket = aws_s3_bucket.static_website.id
 
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Action = [
-          "s3:GetObject"
-        ],
-        Resource = [
-          "${aws_s3_bucket.static_website.arn}/*"
-        ]
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static_website.arn}/*"
       }
     ]
   })
 }
 
-# Crear un API Gateway
-resource "aws_api_gateway_rest_api" "static_site_api" {
-  name = "static-site-api"
+# Crear un API Gateway HTTP no REST
+resource "aws_apigatewayv2_api" "static_site_api" {
+  name          = "static-site-api"
+  protocol_type = "HTTP"
 }
 
-resource "aws_api_gateway_resource" "site" {
-  rest_api_id = aws_api_gateway_rest_api.static_site_api.id
-  parent_id   = aws_api_gateway_rest_api.static_site_api.root_resource_id
-  path_part   = "site"
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                    = aws_apigatewayv2_api.static_site_api.id
+  integration_type          = "AWS_PROXY"
+  integration_method        = "POST"
+  integration_uri           = aws_lambda_function.static_site_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_method" "get" {
-  rest_api_id   = aws_api_gateway_rest_api.static_site_api.id
-  resource_id   = aws_api_gateway_resource.site.id
-  http_method   = "GET"
-  authorization = "NONE"
+resource "aws_apigatewayv2_route" "api_route" {
+  api_id    = aws_apigatewayv2_api.static_site_api.id
+  route_key = "$default"
+
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.static_site_api.id
-  resource_id = aws_api_gateway_resource.site.id
-  http_method = aws_api_gateway_method.get.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.static_site_lambda.invoke_arn
-}
-
-# Permiso para que API Gateway invoque la función Lambda
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.static_site_lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.static_site_api.execution_arn}/*"
+  source_arn = "${aws_apigatewayv2_api.static_site_api.execution_arn}/*"
 }
 
 data "aws_region" "current" {}
@@ -223,14 +207,14 @@ resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version   ="2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
+        Effect    = "Allow",
         Principal = {
           Service = "lambda.amazonaws.com"
-        }
+        },
+        Action    = "sts:AssumeRole"
       }
     ]
   })
@@ -240,13 +224,32 @@ resource "aws_iam_role" "lambda_exec" {
 resource "aws_iam_policy_attachment" "lambda_policy" {
   name       = "lambda_policy_attach"
   roles      = [aws_iam_role.lambda_exec.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  policy_arn = aws_iam_policy.lambda_s3_policy.arn  # Cambiamos al ARN de la política de S3
 }
 
 # Crear la política de ejecución de Lambda
+resource "aws_iam_policy" "lambda_s3_policy" {
+  name        = "lambda-s3-policy"
+  description = "Policy for Lambda to access S3"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = "${aws_s3_bucket.static_website.arn}/*"
+      }
+    ]
+  })
+}
+
 data "aws_iam_policy_document" "lambda_policy" {
   statement {
-    actions = ["lambda:InvokeFunction"]
+    actions   = ["lambda:InvokeFunction"]
     resources = [aws_lambda_function.static_site_lambda.arn]
   }
 }
@@ -257,10 +260,10 @@ resource "aws_lambda_permission" "invoke_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.static_site_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = aws_api_gateway_rest_api.static_site_api.execution_arn
+  source_arn    = "${aws_apigatewayv2_api.static_site_api.execution_arn}/*"
 }
 
 # Probar el Sitio Institucional
 output "site_url" {
-  value = "https://${aws_api_gateway_rest_api.static_site_api.id}.execute-api.${data.aws_region.current.name}.amazonaws.com"
+  value = "${aws_apigatewayv2_api.static_site_api.api_endpoint}/site"
 }
