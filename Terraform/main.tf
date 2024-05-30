5499a3b151381f0b109fb9cee1f63a7b07a5d48c
Santiago Diaz <santiagodiaz9822@gmail.com>
2024-05-30 11:05:49 -0300

    static web site funcionando

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -107,6 +107,12 @@ resource "aws_lambda_function" "static_site_lambda" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
+
+  environment {
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.static_website.bucket
+    }
+  }
 }
 
 # Crear el rol IAM para la ejecución de la función Lambda
--- scripts/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("El documento HTML ha sido completamente cargado y parseado.");
-  // Puedes agregar más código JavaScript aquí
-});
