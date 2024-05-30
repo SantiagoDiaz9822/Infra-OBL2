6d24ac2cc3c88c571afc9135142aee2ee579adb0
Santiago Diaz <santiagodiaz9822@gmail.com>
2024-05-30 19:56:28 -0300

    clean up

--- Terraform/modules/cloudfront/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "bucket1" {
-  bucket = var.bucket_name
-  force_destroy = var.force_destroy
-}
--- Terraform/modules/s3/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "bucket_name" {
-  description = "Name of the bucket."
-}
-
-variable "force_destroy" {
-  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error."
-}
